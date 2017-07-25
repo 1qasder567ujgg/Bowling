0c7178eaa6637399ffeb3260b624bd4bc09ce1f4
Max Chukavin <max@menatwork.com.ua>
2017-07-25 20:31:07 +0300

    Number_of_Player

--- bowling.py
+++ bowling.py
@@ -2,3 +2,13 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.points = []
+
+
+players = []
+players_count = int(input("How many players? "))
+
+for i in range(players_count):
+    players.append(Player(input("Enter name of " + str(i + 1) + " player: ")))
+
+for player in players:
+    print(player.name)
