90ffe34e4a66120bf2ed490a6c6586af9c8056c1
Max Chukavin <max@menatwork.com.ua>
2017-07-26 22:35:21 +0300

    Fancy Screen

--- bowling.py
+++ bowling.py
@@ -7,8 +7,8 @@ class Player:
 def getNumberOfPlayers():
     while True:
         try:
-            pl_cnt = int(input("How many players? (Up to 5) "))
-            if 0 < pl_cnt < 6:
+            pl_cnt = int(input("How many players? Up to 5. 0 to exit "))
+            if 0 <= pl_cnt <= 5:
                 break
         except:
             pass
@@ -29,6 +29,39 @@ def getPlayers(pl_cnt):
         p.append(Player(p_name))
     return p
 
+def setScreen(players):
+    print("\033[2J", end="")
+    print("\033[H", end="")
+    print("\033[3B", end="")
+
+    i = 1
+    for player in players:
+        print("\033[1B", end="")
+        print('Player #' + str(i) + ': ' + player.name)
+        #print("\033[1A", end="")
+        #print(' __'*21)
+        print('|__' * 21 + '|')
+        print('|_____' * 9 + '|________|')
+        i += 1
+
+def showScore(player, order, round):
+    score_up = ''
+    score_down = ''
+    for p in player.points:
+        score1 = '_' + str(p[0])
+        score2 = '_' + str(p[1])
+        score_up += '|' + score1[-2:] + '|' + score2[-2:]
+        score1 = '__' + str(p[2]) + '__'
+        score_down += '|' + score1[:5] 
+
+    print("\033[H", end="")
+    print("\033[" + str(5 + 4*(order-1)) + "B", end="")
+    print(score_up, end='')
+    print("\033[1B", end="")
+    print("\033[" + str(7 + 4*(order-1)) + ";1H", end="")
+    print(score_down, end='')
+
+
 
 NUMBER_OF_ROUNDS = 3
 
@@ -37,21 +70,32 @@ print("\033[2J", end="")
 print("\033[H", end="")
 
 players_count = getNumberOfPlayers()
-players = getPlayers(players_count)
+if players_count != 0:
+    players = getPlayers(players_count)
 
-for i in range(NUMBER_OF_ROUNDS):
-    for player in players:
+    setScreen(players)
+    print("\033[H", end="")
 
-        first_trow = int(input(player.name + ", what is your 1st score? "))
-        if first_trow == 10:
-            player.points.append([first_trow, 0, "X"])
-        else:
-            second_throw =  int(input(player.name + ", what is your 2nd score? "))
-            if first_trow + second_throw == 10:
-                player.points.append([first_trow, second_throw, "/"])
+    for i in range(NUMBER_OF_ROUNDS):
+        p = 1
+        for player in players:
+            score_msg = 'Round #' + str(i + 1) + ': ' + player.name
+            print("\033[H", end="")
+            print("\033[2K", end="")
+            first_trow = int(input(score_msg + ", what is your 1st score? "))
+            if first_trow == 10:
+                player.points.append([first_trow, 0, "X"])
+                showScore(player, p, i)
             else:
-                player.points.append([first_trow, second_throw, first_trow + second_throw])
-        #print(player.name, player.points)
+                print("\033[H", end="")
+                print("\033[2K", end="")
+                second_throw =  int(input(score_msg + ", what is your 2nd score? "))
+                if first_trow + second_throw == 10:
+                    player.points.append([first_trow, second_throw, "/"])
+                    showScore(player, p, i)
+                else:
+                    player.points.append([first_trow, second_throw, first_trow + second_throw])
+                    showScore(player, p, i)
+            p += 1
 
-for player in players:
-    print(player.name, player.points)
+print("\033[1B", end="\r")
