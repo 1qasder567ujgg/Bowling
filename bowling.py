0601cc19da76dac9d52b357a18c20e154559d4b3
Max Chukavin <max@menatwork.com.ua>
2017-07-27 20:57:20 +0300

    Final roud added

--- bowling.py
+++ bowling.py
@@ -44,14 +44,21 @@ def setScreen(players):
         print('|_____' * 9 + '|________|')
         i += 1
 
-def showScore(player, order, round):
+def showScore(player, order, is_last):
     score_up = ''
     score_down = ''
     for p in player.points:
         score1 = '_' + str(p[0])
         score2 = '_' + str(p[1])
-        score_up += '|' + score1[-2:] + '|' + score2[-2:]
-        score1 = '__' + str(p[2]) + '__'
+        score3 = ''
+        if is_last and len(p) == len(player.points[-1]):
+            score3 = '_' + str(p[2])
+            score3 = '|' + score3[-2:]
+        score_up += '|' + score1[-2:] + '|' + score2[-2:] + score3
+        if is_last and len(p) == len(player.points[-1]):
+            score1 = '__' + str(p[3]) + '__'
+        else:
+            score1 = '__' + str(p[2]) + '__'
         score_down += '|' + score1[:5] 
 
     print("\033[H", end="")
@@ -61,9 +68,24 @@ def showScore(player, order, round):
     print("\033[" + str(7 + 4*(order-1)) + ";1H", end="")
     print(score_down, end='')
 
+def getThrow(msg):
 
+    print("\033[H", end="")
+    print("\033[2K", end="")
+    
+    while True:
+        try:
+            score = int(input(msg))
+            if 0 <= score <= 10:
+                break
+        except:
+            pass
+        print("\033[1A", end="")
+        print("\033[K", end="")
 
-NUMBER_OF_ROUNDS = 3
+    return score
+
+NUMBER_OF_ROUNDS = 10
 
 #Clear screen & move to the top
 print("\033[2J", end="")
@@ -80,22 +102,44 @@ if players_count != 0:
         p = 1
         for player in players:
             score_msg = 'Round #' + str(i + 1) + ': ' + player.name
-            print("\033[H", end="")
-            print("\033[2K", end="")
-            first_trow = int(input(score_msg + ", what is your 1st score? "))
-            if first_trow == 10:
+            first_trow = getThrow(score_msg + ", what is your 1st score? ")
+
+            if i > 0:
+                if player.points[-1][2] == 'X':
+                    if i > 1:
+                        if player.points[-2][2] == 'X' and len(player.points) > 1:
+                            player.points[-2][2] = 20 + first_trow
+
+            if i > 0:
+                if player.points[-1][2] == '/' and player.points:
+                    player.points[-1][2] = 10 + first_trow
+
+
+            if first_trow == 10 and i < 9:
                 player.points.append([first_trow, 0, "X"])
-                showScore(player, p, i)
+                showScore(player, p, False)
             else:
-                print("\033[H", end="")
-                print("\033[2K", end="")
-                second_throw =  int(input(score_msg + ", what is your 2nd score? "))
-                if first_trow + second_throw == 10:
-                    player.points.append([first_trow, second_throw, "/"])
-                    showScore(player, p, i)
+                second_throw =  getThrow(score_msg + ", what is your 2nd score? ")
+
+                if i > 0:
+                    if player.points[-1][2] == 'X' and player.points:
+                        player.points[-1][2] = 10 + first_trow + second_throw
+
+                if i < 9:
+                    if first_trow + second_throw == 10:
+                        player.points.append([first_trow, second_throw, "/"])
+                        showScore(player, p, False)
+                    else:
+                        player.points.append([first_trow, second_throw, first_trow + second_throw])
+                        showScore(player, p, False)
                 else:
-                    player.points.append([first_trow, second_throw, first_trow + second_throw])
-                    showScore(player, p, i)
+                    if first_trow == 10 or (first_trow + second_throw == 10):
+                        tird_throw =  getThrow(score_msg + ", what is your 3rd score? ")
+                        player.points.append([first_trow, second_throw, tird_throw, first_trow + second_throw + tird_throw])
+                    else:
+                        player.points.append([first_trow, second_throw, '', first_trow + second_throw])
+                    showScore(player, p, True)
+                    
             p += 1
 
 print("\033[3B", end="\r")
